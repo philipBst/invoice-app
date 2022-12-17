2cf0b80ebca77e7b4256820e4972f9aeec23bb37
Philip Basnet <philipbasnet@gmail.com>
2022-12-17 19:39:31 +0545

    feat: add actions to invoice context

--- src/contexts/InvoiceContext.tsx
+++ src/contexts/InvoiceContext.tsx
@@ -1,17 +1,28 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useReducer } from 'react'
+
 import { HookUsedOutsideOfContextError } from '../errors'
+
+import { getAllInvoices } from '../services/invoice.service'
 
 import type { IInvoice } from '../interfaces'
 
 export type InvoiceContextValue = {
   invoices: IInvoice[]
+  dispatch: React.Dispatch<IInvoice[]>
 }
 
 const InvoiceContext = createContext<InvoiceContextValue | null>(null)
 
+const invoiceReducer = (_prevState: IInvoice[], newState: IInvoice[]) =>
+  newState
+
 const InvoiceProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const [invoices, dispatch] = useReducer(invoiceReducer, [])
+
   return (
-    <InvoiceContext.Provider value={null}>{children}</InvoiceContext.Provider>
+    <InvoiceContext.Provider value={{ invoices, dispatch }}>
+      {children}
+    </InvoiceContext.Provider>
   )
 }
 
@@ -20,6 +31,11 @@ export function useInvoice() {
   if (!context)
     throw new HookUsedOutsideOfContextError('useInvoice', 'InvoiceContext')
   return context
+}
+
+export async function getInvoices(dispatch: React.Dispatch<IInvoice[]>) {
+  const invoices = await getAllInvoices()
+  dispatch(invoices)
 }
 
 export default InvoiceProvider
