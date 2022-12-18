a6560ce63480fdac38995682bf5ae4fefb8aa3ee
Philip Basnet <philipbasnet@gmail.com>
2022-12-18 15:21:06 +0545

    feat: update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      spartan: ['League Spartan'],
+    },
+    extend: {
+      colors: {
+        'sys-color-1': '#151725',
+        'sys-color-2': '#1f213a',
+        'sys-color-3': '#7c5cf8',
+        'sys-color-4': '#494f6f',
+        'sys-color-5': '#b2b4ce',
+        'sys-color-6': '#7a85bf',
+        'sys-color-7': '#33d79e',
+      },
+    },
   },
   plugins: [],
 }
