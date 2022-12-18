5b751b464a7de9bf9ca53bd9184586be19db1257
Philip Basnet <philipbasnet@gmail.com>
2022-12-18 15:19:19 +0545

    feat: get all invoices from hook

--- src/pages/InvoicesPage.tsx
+++ src/pages/InvoicesPage.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from 'react'
+import { getInvoices, useInvoice } from '../contexts/InvoiceContext'
+
 const InvoicesPage = () => {
+  const { invoices, dispatch } = useInvoice()
+  console.log(invoices)
+  useEffect(() => {
+    getInvoices(dispatch)
+  }, [dispatch])
   return <>invoices</>
 }
 
