895f7d142967fbb23574225d0126209c40f18214
Philip Basnet <philipbasnet@gmail.com>
2022-12-18 15:16:14 +0545

    feat: clone root data to be immutable

--- src/services/invoice.service.ts
+++ src/services/invoice.service.ts
@@ -1,6 +1,8 @@
-import invoices from '../data/invoices.json'
+import data from '../data/invoices.json'
 
 import type { IInvoice } from '../interfaces'
+
+const invoices = JSON.parse(JSON.stringify(data))
 
 export async function getAllInvoices() {
   return invoices as IInvoice[]
