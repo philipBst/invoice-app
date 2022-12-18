f56da43562f033bea20f93fdbdbb4cd324c467ca
Philip Basnet <philipbasnet@gmail.com>
2022-12-18 16:53:16 +0545

    feat: create invoice staus chip component

---
+++ src/components/InvoiceStatusChip.tsx
@@ -0,0 +1,3 @@
+const InvoiceStatusChip = () => <></>
+
+export default InvoiceStatusChip
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Sidebar } from './Sidebar'
 export { default as Invoicebar } from './Invoicebar'
+export { default as InvoiceStatusChip } from './InvoiceStatusChip'
