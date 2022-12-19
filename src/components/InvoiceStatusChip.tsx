3614a537b6dac2ad56178ca15254c31d54e91825
Philip Basnet <philipbasnet@gmail.com>
2022-12-19 14:10:17 +0545

    fix: fix classname issue

--- src/components/InvoiceStatusChip.tsx
+++ src/components/InvoiceStatusChip.tsx
@@ -1,3 +1,48 @@
-const InvoiceStatusChip = () => <></>
+import classnames from 'classnames'
+
+import type { InvoiceStatus } from '../types'
+
+export type InvoiceStatusChipProps = {
+  status: InvoiceStatus
+}
+
+const InvoiceStatusChip: React.FC<InvoiceStatusChipProps> = ({ status }) => {
+  const isDraft = status === 'draft'
+  const isPending = status === 'pending'
+  const isPaid = status === 'paid'
+
+  return (
+    <span
+      className={classnames(
+        'flex',
+        'items-center',
+        'justify-center',
+        'gap-3',
+        'rounded-md',
+        'py-2',
+        'px-6',
+        'font-bold',
+        'capitalize',
+        {
+          'bg-sys-color-7/10': isPaid,
+          'text-sys-color-7': isPaid,
+          'bg-sys-color-9/10': isPending,
+          'text-sys-color-9': isPending,
+          'bg-sys-color-10/10': isDraft,
+          'text-sys-color-10': isDraft,
+        },
+      )}
+    >
+      <span
+        className={classnames('h-2', 'w-2', 'rounded-full', {
+          'bg-sys-color-7': isPaid,
+          'bg-sys-color-9': isPending,
+          'bg-sys-color-10': isDraft,
+        })}
+      ></span>
+      {status}
+    </span>
+  )
+}
 
 export default InvoiceStatusChip
