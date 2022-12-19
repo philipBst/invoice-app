c7a2ec7035b31614b040abd4fc42084d03d91ebb
Philip Basnet <philipbasnet@gmail.com>
2022-12-19 15:23:23 +0545

    feat: add service to filter invoices by status

--- src/services/invoice.service.ts
+++ src/services/invoice.service.ts
@@ -1,9 +1,14 @@
 import data from '../data/invoices.json'
 
 import type { IInvoice } from '../interfaces'
+import type { InvoiceStatus } from '../types/InvoiceStatus.type'
 
-const invoices = JSON.parse(JSON.stringify(data))
+const invoices: IInvoice[] = JSON.parse(JSON.stringify(data))
 
 export async function getAllInvoices() {
-  return invoices as IInvoice[]
+  return invoices
+}
+
+export async function getAllInvoicesByStatus(status: InvoiceStatus) {
+  return invoices.filter(invoice => invoice.status === status)
 }
