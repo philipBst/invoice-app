d2509c2b934f7a59b5822571a4c275feda2e505e
Philip Basnet <philipbasnet@gmail.com>
2022-12-20 20:22:34 +0545

    fix: fix type for dialog props

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -5,10 +5,19 @@ export type DialogProps = {} & React.DetailedHTMLProps<
   HTMLDialogElement
 >
 
-const Dialog: React.ForwardRefExoticComponent<
-  React.RefAttributes<HTMLDialogElement>
-> = forwardRef(
-  ({ children, ...props }: React.PropsWithChildren<DialogProps>, ref) => (
+const Dialog = forwardRef(
+  (
+    {
+      children,
+      ...props
+    }: React.PropsWithChildren<
+      React.DetailedHTMLProps<
+        React.DialogHTMLAttributes<HTMLDialogElement>,
+        HTMLDialogElement
+      >
+    >,
+    ref: React.ForwardedRef<HTMLDialogElement>,
+  ) => (
     <dialog ref={ref} {...props}>
       {children}
     </dialog>
