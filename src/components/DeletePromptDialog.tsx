ea31418f2f4309ff73b733e01282e6d0752061a4
Philip Basnet <philipbasnet@gmail.com>
2022-12-21 12:59:55 +0545

    feat: make return implicit

--- src/components/DeletePromptDialog.tsx
+++ src/components/DeletePromptDialog.tsx
@@ -15,33 +15,31 @@ const DeletePromptDialog = forwardRef(
   (
     { id, onDelete, onDeleteCancel, ...props }: DeletePromptDialogProps,
     ref?: React.ForwardedRef<HTMLDialogElement>,
-  ) => {
-    return (
-      <Dialog id={id} {...props} ref={ref}>
-        <p className="font-bold text-2xl">Confirm Deletion</p>
-        <p>
-          Are you sure you want to delete invoice #{id}? This action cannot be
-          undone.
-        </p>
-        <form method="dialog" className="w-full text-right space-x-4">
-          <button
-            className="bg-sys-color-12 rounded-full py-3 px-5"
-            value="submit"
-            onClick={onDeleteCancel}
-          >
-            Cancel
-          </button>
-          <button
-            className="bg-sys-color-13 rounded-full py-3 px-5"
-            value="submit"
-            onClick={onDelete}
-          >
-            Delete
-          </button>
-        </form>
-      </Dialog>
-    )
-  },
+  ) => (
+    <Dialog id={id} {...props} ref={ref}>
+      <p className="font-bold text-2xl">Confirm Deletion</p>
+      <p>
+        Are you sure you want to delete invoice #{id}? This action cannot be
+        undone.
+      </p>
+      <form method="dialog" className="w-full text-right space-x-4">
+        <button
+          className="bg-sys-color-12 rounded-full py-3 px-5"
+          value="submit"
+          onClick={onDeleteCancel}
+        >
+          Cancel
+        </button>
+        <button
+          className="bg-sys-color-13 rounded-full py-3 px-5"
+          value="submit"
+          onClick={onDelete}
+        >
+          Delete
+        </button>
+      </form>
+    </Dialog>
+  ),
 )
 
 export default DeletePromptDialog
