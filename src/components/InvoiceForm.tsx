31edc39b72a171947e53c5b9b9bca1900930c148
Philip Basnet <philipbasnet@gmail.com>
2022-12-21 15:47:05 +0545

    feat: use invoice input

--- src/components/InvoiceForm.tsx
+++ src/components/InvoiceForm.tsx
@@ -1,3 +1,5 @@
+import InvoiceInput from './InvoiceInput'
+
 const InvoiceForm = () => {
   return (
     <section className="ml-20 w-96 bg-sys-color-1 absolute p-10 pl-14 top-0 left-0 bottom-0 rounded-tr-3xl">
@@ -5,43 +7,11 @@ const InvoiceForm = () => {
       <div className="space-y-8">
         <form className="space-y-4 overflow-y-auto">
           <p className="text-sm text-sys-color-3">Bill From</p>
-          <div className="flex flex-col gap-2 w-full">
-            <label htmlFor="" className="text-xs text-sys-color-16">
-              Street Address
-            </label>
-            <input
-              type="text"
-              className="border border-sys-color-12 w-full outline-none bg-sys-color-2 rounded-sm px-2 py-1 text-white"
-            />
-          </div>
+          <InvoiceInput label="Street Address" />
           <div className="flex gap-2">
-            <div className="flex flex-col gap-2 w-full">
-              <label htmlFor="" className="text-xs text-sys-color-16">
-                Street Address
-              </label>
-              <input
-                type="text"
-                className="w-full outline-none bg-sys-color-2 rounded-sm px-2 py-1 text-white"
-              />
-            </div>
-            <div className="flex flex-col gap-2 w-full">
-              <label htmlFor="" className="text-xs text-sys-color-16">
-                Street Address
-              </label>
-              <input
-                type="text"
-                className="w-full outline-none bg-sys-color-2 rounded-sm px-2 py-1 text-white"
-              />
-            </div>
-            <div className="flex flex-col gap-2 w-full">
-              <label htmlFor="" className="text-xs text-sys-color-16">
-                Street Address
-              </label>
-              <input
-                type="text"
-                className="w-full outline-none bg-sys-color-2 rounded-sm px-2 py-1 text-white"
-              />
-            </div>
+            <InvoiceInput label="City" />
+            <InvoiceInput label="Post Code" />
+            <InvoiceInput label="Country" />
           </div>
         </form>
       </div>
