dfa0874d87706f085e8d3bee3f3daef370bf666a
Philip Basnet <philipbasnet@gmail.com>
2022-12-21 22:32:09 +0545

    feat: create box element

---
+++ src/components/Box.tsx
@@ -0,0 +1,29 @@
+import { forwardRef } from 'react'
+import type { HTMLBlockElements } from '../types'
+
+const Box = forwardRef(
+  (
+    {
+      as,
+      children,
+      ...props
+    }: React.PropsWithChildren<
+      {
+        as?: HTMLBlockElements
+      } & React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLDivElement>,
+        HTMLDivElement
+      >
+    >,
+    ref: React.ForwardedRef<HTMLDivElement>,
+  ) => {
+    const Box = as || 'div'
+    return (
+      <Box {...props} ref={ref}>
+        {children}
+      </Box>
+    )
+  },
+)
+
+export default Box
