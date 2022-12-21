fc7e2c197ea8b4b2be336a06c2b3a139d00380e6
Philip Basnet <philipbasnet@gmail.com>
2022-12-21 21:03:44 +0545

    feat: use plusicon instead of plain svg

--- src/pages/InvoicesPage.tsx
+++ src/pages/InvoicesPage.tsx
@@ -8,6 +8,7 @@ import {
 } from '../contexts/InvoiceContext'
 
 import { Invoicebar, SlideRight, InvoiceForm } from '../components'
+import { PlusIcon } from '../components/icons'
 
 import type { InvoiceStatus } from '../types'
 import type { IInvoice } from '../interfaces'
@@ -86,13 +87,7 @@ const InvoicesPage = () => {
               onClick={openInvoiceForm}
             >
               <span className="flex h-8 w-8 items-center justify-center rounded-full bg-white">
-                <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M6.313 10.023v-3.71h3.71v-2.58h-3.71V.023h-2.58v3.71H.023v2.58h3.71v3.71z"
-                    fill="#7C5DFA"
-                    fillRule="nonzero"
-                  />
-                </svg>
+                <PlusIcon />
               </span>
               <span className="text-sm font-bold">New Invoice</span>
             </button>
