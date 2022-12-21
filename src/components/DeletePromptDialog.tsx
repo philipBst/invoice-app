718520473bbc0f5574728a7f9833842be1e6ada4
Philip Basnet <philipbasnet@gmail.com>
2022-12-21 12:55:27 +0545

    fix: fix prop type ref conflict

--- src/components/DeletePromptDialog.tsx
+++ src/components/DeletePromptDialog.tsx
@@ -1,51 +1,47 @@
-import { useEffect, useRef } from 'react'
+import { forwardRef } from 'react'
 import Dialog from './Dialog'
 
 export type DeletePromptDialogProps = {
-  id: string
-  open: boolean
-  onConfirm?: () => void
-  onCancel?: () => void
-}
+  onDelete?: () => void
+  onDeleteCancel?: () => void
+} & React.PropsWithChildren<
+  React.DetailedHTMLProps<
+    React.DialogHTMLAttributes<HTMLDialogElement>,
+    HTMLDialogElement
+  >
+>
 
-const DeletePromptDialog: React.FC<
-  React.PropsWithChildren<DeletePromptDialogProps>
-> = ({ id, open, onConfirm, onCancel }) => {
-  const dialogRef = useRef<HTMLDialogElement>(null)
-
-  useEffect(() => {
-    if (open) {
-      dialogRef.current?.showModal()
-    } else {
-      dialogRef.current?.close()
-    }
-  }, [open])
-
-  return (
-    <Dialog ref={dialogRef}>
-      <p className="font-bold text-2xl">Confirm Deletion</p>
-      <p>
-        Are you sure you want to delete invoice #{id}? This action cannot be
-        undone.
-      </p>
-      <form method="dialog" className="w-full text-right space-x-4">
-        <button
-          className="bg-sys-color-12 rounded-full py-3 px-5"
-          value="submit"
-          onClick={onCancel}
-        >
-          Cancel
-        </button>
-        <button
-          className="bg-sys-color-13 rounded-full py-3 px-5"
-          value="submit"
-          onClick={onConfirm}
-        >
-          Delete
-        </button>
-      </form>
-    </Dialog>
-  )
-}
+const DeletePromptDialog = forwardRef(
+  (
+    { id, onDelete, onDeleteCancel, ...props }: DeletePromptDialogProps,
+    ref?: React.ForwardedRef<HTMLDialogElement>,
+  ) => {
+    return (
+      <Dialog id={id} {...props} ref={ref}>
+        <p className="font-bold text-2xl">Confirm Deletion</p>
+        <p>
+          Are you sure you want to delete invoice #{id}? This action cannot be
+          undone.
+        </p>
+        <form method="dialog" className="w-full text-right space-x-4">
+          <button
+            className="bg-sys-color-12 rounded-full py-3 px-5"
+            value="submit"
+            onClick={onDeleteCancel}
+          >
+            Cancel
+          </button>
+          <button
+            className="bg-sys-color-13 rounded-full py-3 px-5"
+            value="submit"
+            onClick={onDelete}
+          >
+            Delete
+          </button>
+        </form>
+      </Dialog>
+    )
+  },
+)
 
 export default DeletePromptDialog
