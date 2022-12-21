ff4d90b4d2962966763c96559d573a56c095e5c5
Philip Basnet <philipbasnet@gmail.com>
2022-12-21 18:31:14 +0545

    feat: register input with react-hook-form

--- src/components/InvoiceForm.tsx
+++ src/components/InvoiceForm.tsx
@@ -1,3 +1,5 @@
+import { useForm } from 'react-hook-form'
+
 import InvoiceInput from './InvoiceInput'
 
 import type { IInvoice } from '../interfaces'
@@ -5,7 +7,7 @@ import type { IInvoice } from '../interfaces'
 export type InvoiceFormProps = {
   action: 'new' | 'edit'
   invoiceId?: string
-  invoice?: IInvoice
+  invoice?: IInvoice | undefined
   onSaveAsDraft?: () => void
   onCancel?: () => void
   onSave?: () => void
@@ -17,39 +19,85 @@ const InvoiceForm: React.FC<InvoiceFormProps> = ({
   onSaveAsDraft,
   onCancel,
   onSave,
+  invoice = {},
 }) => {
+  const { register, handleSubmit, getValues } = useForm<IInvoice>({
+    defaultValues: {
+      ...invoice,
+    },
+  })
   return (
     <section className="ml-20 w-[30rem] space-y-6 bg-sys-color-1 absolute p-10 pr-4 pt-8 pl-14 top-0 left-0 bottom-0 rounded-tr-3xl">
       <p className="font-bold text-xl">
         {action === 'new' ? 'New Invoice' : `Edit #${invoiceId || ''}`}
       </p>
-      <form className="space-y-6 h-[calc(100vh-11rem)] pr-4 overflow-y-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-sys-color-12 scrollbar-track-transparent">
+      <div className="space-y-6 h-[calc(100vh-11rem)] pr-4 overflow-y-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-sys-color-12 scrollbar-track-transparent">
         <div className="space-y-4">
           <p className="text-sm text-sys-color-3">Bill From</p>
-          <InvoiceInput label="Street Address" />
+          <InvoiceInput
+            label="Street Address"
+            {...register('senderAddress.street', { required: true })}
+          />
           <div className="flex gap-2">
-            <InvoiceInput label="City" />
-            <InvoiceInput label="Post Code" />
-            <InvoiceInput label="Country" />
+            <InvoiceInput
+              label="City"
+              {...register('senderAddress.city', { required: true })}
+            />
+            <InvoiceInput
+              label="Post Code"
+              {...register('senderAddress.postCode', { required: true })}
+            />
+            <InvoiceInput
+              label="Country"
+              {...register('senderAddress.country', { required: true })}
+            />
           </div>
         </div>
         <div className="space-y-4">
           <p className="text-sm text-sys-color-3">Bill To</p>
-          <InvoiceInput label="Client's name" />
-          <InvoiceInput label="Client's email" />
-          <InvoiceInput label="Street Address" />
+          <InvoiceInput
+            label="Client's name"
+            {...register('clientName', { required: true })}
+          />
+          <InvoiceInput
+            label="Client's email"
+            {...register('clientEmail', { required: true })}
+          />
+          <InvoiceInput
+            label="Street Address"
+            {...register('clientAddress.street', { required: true })}
+          />
           <div className="flex gap-2">
-            <InvoiceInput label="City" />
-            <InvoiceInput label="Post Code" />
-            <InvoiceInput label="Country" />
+            <InvoiceInput
+              label="City"
+              {...register('clientAddress.city', { required: true })}
+            />
+            <InvoiceInput
+              label="Post Code"
+              {...register('clientAddress.postCode', { required: true })}
+            />
+            <InvoiceInput
+              label="Country"
+              {...register('clientAddress.country', { required: true })}
+            />
           </div>
         </div>
         <div className="space-y-4">
           <div className="flex gap-2">
-            <InvoiceInput label="Invoice Date" type="date" />
-            <InvoiceInput label="Payment Terms" />
+            <InvoiceInput
+              label="Invoice Date"
+              type="date"
+              {...register('createdAt', { required: true })}
+            />
+            <InvoiceInput
+              label="Payment Terms"
+              {...register('paymentTerms', { required: false })}
+            />
           </div>
-          <InvoiceInput label="Project Description" />
+          <InvoiceInput
+            label="Project Description"
+            {...register('description', { required: true })}
+          />
         </div>
         <div className="space-y-4">
           <p className="text-sm text-sys-color-16">Item List</p>
@@ -68,7 +116,7 @@ const InvoiceForm: React.FC<InvoiceFormProps> = ({
             + Add New Item
           </button>
         </div>
-      </form>
+      </div>
       <div className="flex items-center justify-between bg-sys-color-1 w-full text-sm">
         {action === 'new' ? (
           <button
@@ -83,13 +131,19 @@ const InvoiceForm: React.FC<InvoiceFormProps> = ({
         <div className="flex items-center gap-4">
           <button
             className="rounded-full bg-sys-color-17 text-sys-color-10 py-3 px-5"
-            onClick={onSaveAsDraft || onCancel}
+            onClick={() => {
+              const invoice = getValues()
+              console.log(invoice, 'draft')
+            }}
           >
             {action === 'new' ? 'Save as Draft' : 'Cancel'}
           </button>
           <button
             className="rounded-full bg-sys-color-3 text-white py-3 px-5"
-            onClick={onSave}
+            onClick={handleSubmit(invoice => {
+              console.log(invoice, 'new')
+              onSave?.()
+            })}
           >
             {action === 'new' ? 'Save & Send' : 'Save Changes'}
           </button>
